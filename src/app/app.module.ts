040e4393d01b8713ab0093ba751a5e29d17560ea
Rafeedovic <rafeedbj1@gmail.com>
2024-05-17 11:27:07 +0200

    1st deployement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getMessaging, provideMessaging } from '@angular/fire/messaging';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
-import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [
@@ -26,19 +18,10 @@ import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
   ],
   providers: [
     provideClientHydration(),
-    provideFirebaseApp(() => initializeApp({"projectId":"voiturecolo","appId":"1:440987894490:web:77aaa0699022fb791d9be3","storageBucket":"voiturecolo.appspot.com","apiKey":"AIzaSyDf8MKQ1cSbbh2Rjc1ZP6ydZAZCKbyi1C0","authDomain":"voiturecolo.firebaseapp.com","messagingSenderId":"440987894490"})),
+    provideFirebaseApp(() => initializeApp({"projectId":"voiturecolo-dec00","appId":"1:1010105974504:web:00078d6abe71f61282935d","storageBucket":"voiturecolo-dec00.appspot.com","apiKey":"AIzaSyBlOUq67hFo0RKcdcqN5VyePZc_WWegEW4","authDomain":"voiturecolo-dec00.firebaseapp.com","messagingSenderId":"1010105974504"})),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideMessaging(() => getMessaging()),
-    providePerformance(() => getPerformance()),
-    provideStorage(() => getStorage()),
-    provideRemoteConfig(() => getRemoteConfig()),
-    provideFunctions(() => getFunctions()),
-    provideAnalytics(() => getAnalytics()),
-    ScreenTrackingService,
-    UserTrackingService,
-    provideDatabase(() => getDatabase()),
-    provideVertexAI(() => getVertexAI())
+    provideStorage(() => getStorage())
   ],
   bootstrap: [AppComponent]
 })
