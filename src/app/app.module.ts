c632255a284e66060beaf49721902f63b7bf35ec
Rafeedovic <rafeedbj1@gmail.com>
2024-05-17 10:00:05 +0200

    1st push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [
@@ -13,7 +25,20 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({"projectId":"voiturecolo","appId":"1:440987894490:web:77aaa0699022fb791d9be3","storageBucket":"voiturecolo.appspot.com","apiKey":"AIzaSyDf8MKQ1cSbbh2Rjc1ZP6ydZAZCKbyi1C0","authDomain":"voiturecolo.firebaseapp.com","messagingSenderId":"440987894490"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideFunctions(() => getFunctions()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideDatabase(() => getDatabase()),
+    provideVertexAI(() => getVertexAI())
   ],
   bootstrap: [AppComponent]
 })
