79ea2b1ea5e6449e59dbb9aa7a72be3417156241
Rafeedovic <rafeedbj1@gmail.com>
2024-06-06 11:54:16 +0200

    inscription + verif inscri

--- src/app/components/inscription/inscription.component.ts
+++ src/app/components/inscription/inscription.component.ts
@@ -7,9 +7,14 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
+
+
+
 export class InscriptionComponent {
+  submitted: boolean = false;
   registerForm: FormGroup;
   errorMessage: string = '';
+  successMessage: string = '';
   userInfo: { nom: string, prenom: string, email: string } | null = null;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
@@ -22,23 +27,27 @@ export class InscriptionComponent {
   }
 
   async onSubmit() {
+    this.submitted = true;
     if (this.registerForm.valid) {
       const { email, password, nom, prenom } = this.registerForm.value;
       try {
-        const result = await this.authService.register(email, password);
+        // Inscription de l'utilisateur
+        await this.authService.register(email, password);
         // Stocker les informations de l'utilisateur
         this.userInfo = {
           email: email,
           nom: nom,
           prenom: prenom
         };
-        this.errorMessage = '';
+        this.errorMessage = ''; // Réinitialiser le message d'erreur
+        this.successMessage = 'Votre inscription est bien validée '; // Afficher le message de succès
       } catch (error) {
         if (error instanceof Error) {
           this.errorMessage = error.message;
         } else {
-          this.errorMessage = "An unknown error occurred";
+          this.errorMessage = "Une erreur inconnue s'est produite.";
         }
+        this.successMessage = ''; // Réinitialiser le message de succès
       }
     }
   }
