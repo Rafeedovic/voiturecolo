2fe1d9669a792a17f7dfe8b2000bc3733512bbd3
Rafeedovic <rafeedbj1@gmail.com>
2024-06-14 13:44:17 +0200

    aze

--- src/app/components/listevoitures/listevoitures.component.ts
+++ src/app/components/listevoitures/listevoitures.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ConnexionComponent } from '../connexion/connexion.component';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { VoitureService } from '../../services/voiture.service';
@@ -8,45 +7,45 @@ import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-listevoitures',
   templateUrl: './listevoitures.component.html',
-  styleUrl: './listevoitures.component.css'
+  styleUrls: ['./listevoitures.component.css']
 })
-
-export class ListevoituresComponent {
-  email : any  | null = '';
+export class ListevoituresComponent implements OnInit {
   searchQuery: string = '';
   voitures: any[] = [];
-  firstVoiture: any;
   selectedMarque: string = '';
   marques: string[] = [];
   dataSource = new MatTableDataSource<any>();
-  displayedColumns: string[] = ['marque', 'modele_dossier', 'designation_commerciale', 'carburant','puissance_maximale','boite_de_vitesse','consommation_mixte_l_100km','co2_g_km','annee','carrosserie','gamme'];
+  displayedColumns: string[] = ['marque', 'modele_dossier', 'designation_commerciale', 'carburant', 'puissance_maximale', 'boite_de_vitesse', 'consommation_mixte_l_100km', 'co2_g_km', 'annee', 'carrosserie', 'gamme'];
 
-  constructor(private route: ActivatedRoute,private authService: AuthService,private voitureService: VoitureService,private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private voitureService: VoitureService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    if (this.authService.getUser()){
-      this.email = this.authService.getUser()?.email;
-    };
     this.getVoitures();
   }
 
-  async logout(){
-    const deconnexion = await this.authService.logout();
-    console.log('deconnexion');
-    this.router.navigate(['home']);
-    this.email = '';
+  getVoitures() {
+    this.voitureService.getVoitures().subscribe(
+      data => {
+        this.voitures = data.results;
+        // this.marques = [...new Set(this.voitures.map(voiture => voiture.marque))];
+        // this.filterByMarque();
+      },
+      error => {
+        console.error('Erreur lors de la recherche de voitures:', error);
+      }
+    );
   }
 
-  getVoitures() {
-    if (true) {
-      this.voitureService.getVoitures().subscribe(
-        data => {
-          this.voitures = data.results;
-        },
-        error => {
-          console.error('Erreur lors de la recherche de voitures:', error);
-        }
-      );
-    }
-  }
+  // filterByMarque() {
+  //   if (this.selectedMarque) {
+  //     this.dataSource.data = this.voitures.filter(voiture => voiture.marque === this.selectedMarque);
+  //   } else {
+  //     this.dataSource.data = this.voitures;
+  //   }
+  // }
 }
